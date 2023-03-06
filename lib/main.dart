276cb7497598ed343e2f890727b656f26e7c3f75
Hassna16 <hassna.agouzzal@edu.ece.fr>
2023-03-06 22:20:17 +0100

    page de connexion- maj

--- lib/main.dart
+++ lib/main.dart
@@ -44,7 +44,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-
+/*
     return Scaffold(
       backgroundColor: Colors.black87,
       body:/*Stack (
@@ -208,6 +208,170 @@ class _MyHomePageState extends State<MyHomePage> {
 
       ),
       // This trailing comma makes auto-formatting nicer for build methods.
-    ));
+    ));*/
+  //PAGE INSCRIPTION 2
+    return Scaffold(
+        backgroundColor: Colors.black87,
+        body:
+        Align(
+          alignment: Alignment.topCenter,
+
+          child: FractionallySizedBox(
+            widthFactor: 0.8,
+
+
+            child: Column(
+
+
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+
+                SizedBox.square(
+                  dimension: 50.0,
+                ),
+                Text( 'Inscription',
+                  style: TextStyle(
+                    fontWeight : FontWeight.w600,
+                    color: Colors.white,
+                    fontSize:40,
+
+
+                  ),
+
+                ),
+                SizedBox.square(
+                  dimension: 20.0,
+                ),
+                SizedBox(
+                    height:70,
+                    width: 300,
+                    child  : Text("Veuillez saisir ces différentes informations, "
+                        "afin que vos listes soient sauvegardées.",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize:15,),
+                      textAlign: TextAlign.center,
+
+                    )),
+
+                SizedBox.square(
+                  dimension: 30.0,
+                ),
+                SizedBox(
+                    height: 45,
+                    width: 1500,
+                    child  :TextButton(
+                        onPressed: () {},
+                        child: Row(
+                          children: [
+                            Expanded(child: Text("Nom d'utilisateur",textAlign: TextAlign.center)),
+
+                          ],
+                        ),  style: OutlinedButton.styleFrom(
+                      primary: Colors.white,
+                      shape:  RoundedRectangleBorder(borderRadius: BorderRadius.circular(5),),
+                      backgroundColor: Color.fromRGBO(60, 63, 65, 1.0),
+                    ))),
+
+                SizedBox.square(
+                  dimension: 10.0,
+                ),
+// MOT DE PASSE
+                SizedBox(
+                    height: 45,
+                    width: 1500,
+                    child  :TextButton(
+                        onPressed: () {},
+                        child: Row(
+                          children: [
+                            Expanded(child: Text('E-Mail',textAlign: TextAlign.center)),
+
+                          ],
+                        ),  style: OutlinedButton.styleFrom(
+                      primary: Colors.white,
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5),),
+                      backgroundColor: Color.fromRGBO(60, 63, 65, 1.0),
+                    ))),
+
+
+                SizedBox.square(
+                  dimension: 10.0,
+                ),
+
+                SizedBox(
+                    height: 45,
+                    width: 1500,
+                    child  :TextButton(
+                        onPressed: () {},
+                        child: Row(
+                          children: [
+                            Expanded(child: Text('Mot de passe',textAlign: TextAlign.center)),
+
+                          ],
+                        ),  style: OutlinedButton.styleFrom(
+                      primary: Colors.white,
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5),),
+                      backgroundColor: Color.fromRGBO(60, 63, 65, 1.0),
+                    ))),
+
+
+                SizedBox.square(
+                  dimension: 10.0,
+                ),
+
+                SizedBox(
+                    height: 45,
+                    width: 1500,
+                    child  :TextButton(
+                        onPressed: () {},
+                        child: Row(
+                          children: [
+                            Expanded(child: Text('Vérification du mot de passe',textAlign: TextAlign.center)),
+
+                          ],
+                        ),  style: OutlinedButton.styleFrom(
+                      primary: Colors.white,
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5),),
+                      backgroundColor: Color.fromRGBO(60, 63, 65, 1.0),
+                    ))),
+
+                SizedBox.square(
+                  dimension: 80.0,
+                ),
+
+                SizedBox(
+                    height: 45,
+                    width: 1500,
+                    child  :TextButton(
+                        onPressed: () {},
+                        child: Row(
+                          children: [
+                            Expanded(child: Text("S'inscrire",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize:17,),
+                                textAlign: TextAlign.center)
+                            ),
+
+
+                          ],
+                        ),  style: OutlinedButton.styleFrom(
+                      primary: Colors.white,
+                      shape:  RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      backgroundColor:Color.fromRGBO(97, 104, 237, 1.0),
+                    ))),
+
+
+
+
+
+              ],
+            ),
+
+          ),
+          // This trailing comma makes auto-formatting nicer for build methods.
+        ));
   }
 }
